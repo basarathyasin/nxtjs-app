83125a4462bbbd8b8c3e81f852d54ba01a9d550e
basarathyasin <basarathyasin9@gmail.com>
2026-07-13 13:06:05 +0530

    implememt useQuery instead of apollo client

--- src/services/fetchProjects.ts
+++ src/services/fetchProjects.ts
@@ -1,34 +1,41 @@
 import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 
 import type { Project } from "@/components/platform/ProjectsTable";
-import { apolloClient } from "@/src/libs/apolloClient";
 
 const GET_PROJECTS = gql`
-	query GetProjects {
-		projects {
-			id
-			firstName
-			lastName
-			email
-			company
-			teamSize
-			goal
-			budget
-			timeline
-			createdAt
-		}
-	}
+  query GetProjects {
+    projects {
+      id
+      firstName
+      lastName
+      email
+      company
+      teamSize
+      goal
+      budget
+      timeline
+      createdAt
+    }
+  }
 `;
 
 type ProjectsResponse = {
-	projects: Project[];
+  projects: Project[];
 };
 
-export async function fetchProjects() {
-	const response = await apolloClient.query<ProjectsResponse>({
-		query: GET_PROJECTS,
-		fetchPolicy: "network-only",
-	});
+export function useProjects() {
+  const { data, loading, error, refetch } = useQuery<ProjectsResponse>(
+    GET_PROJECTS,
+    {
+      fetchPolicy: "network-only",
+    }
+  );
 
-	return response.data?.projects ?? [];
+  return {
+    projects: data?.projects ?? [],
+    loading,
+    error,
+    refetch,
+  };
 }
